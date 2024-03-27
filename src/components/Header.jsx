6c3a5eb4297b7d89bb3cb7571a7b302d5e456973
Sonseongoh <nobleser@naver.com>
2024-03-27 23:24:19 +0900

    모달창 체크아웃전까지 완성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,19 @@ import { useContext } from "react";
 import foodLogoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -16,7 +23,9 @@ export default function Header() {
         <h1>음식 주문</h1>
       </div>
       <nav>
-        <Button textOnly>장바구니({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          장바구니({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
